f597414cd649d837515af51c48a881e80bd36601
kernemi <149039271+kernemi@users.noreply.github.com>
2023-11-15 12:20:55 +0300

    Update BMI.cpp

--- ETS0760_15_kernemi_kidane/Exercise_1/1.BMI/BMI.cpp
+++ ETS0760_15_kernemi_kidane/Exercise_1/1.BMI/BMI.cpp
@@ -1,1 +1,19 @@
+//calculate BMI
+#include<iostream>
 
+using namespace std;
+
+int main()
+{ 
+   float W,H,BMI;
+   //W is weight of the person, H is height of the person
+     cout<<"enter the weight"<<endl;
+     cin>>W;
+     cout<<"enter the height"<<endl;
+     cin>>H;
+     BMI=W/(H*H);
+	 cout<<"BMI is"<<BMI<<endl;
+	 
+	 return 0;
+	 
+}
