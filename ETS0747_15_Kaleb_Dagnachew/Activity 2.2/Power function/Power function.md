5f083c37d624e576aa1c74f53292ad7a1d374fb4
Kaleb Dagnachew <149406171+Kaleb-dag@users.noreply.github.com>
2023-11-17 20:30:13 +0300

    Add files via upload

---
+++ ETS0747_15_Kaleb_Dagnachew/Activity 2.2/Power function/Power function.md
@@ -0,0 +1,12 @@
+#4 power function
+##Program analysis
+Input: X and Y
+Output: F
+Operation:  - F=pow(X,Y)
+            -display F
+##Pseudocode 
+Step 1: start
+Step 2: enter X and Y
+Step 3:calculate F as F=pow(X,Y)
+Step 4:display F
+Step 5:end
---
+++ ETS0747_15_Kaleb_Dagnachew/Activity 2.2/Power function/power function.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+    int x, y;
+    cout << "Enter the base number: ";
+    cin >> x;
+    cout << "Enter the exponent: ";
+    cin >> y;
+    int z = pow(x, y);
+    cout << "The result is " << z;
+    return 0;
+}
