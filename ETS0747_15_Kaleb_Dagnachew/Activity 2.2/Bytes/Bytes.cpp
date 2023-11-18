8cb60913685ff12b114de37476ea91f9b4df8220
Kaleb Dagnachew <149406171+Kaleb-dag@users.noreply.github.com>
2023-11-18 09:59:58 +0300

    Bytes.cpp

--- ETS0747_15_Kaleb_Dagnachew/Activity 2.2/Bytes/Bytes.cpp
+++ ETS0747_15_Kaleb_Dagnachew/Activity 2.2/Bytes/Bytes.cpp
@@ -1,1 +1,27 @@
+#include<iostream>
 
+using namespace std;
+
+int main()
+{ 
+  int timeInSec, timeInHour, timeInDay, sizeInByte;
+  
+     cout<<"enter the size in bytes"<<endl;
+     
+	 cin>>sizeInByte;
+     
+     timeInSec=sizeInByte/960; 
+     
+     timeInHour=timeInSec/3600;
+     
+     timeInDay=timeInHour/24;
+     
+	 cout<<"time needed in seconds is "<<timeInSec<<endl;
+	 
+	 cout<<"time needed in hours is "<<timeInHour<<endl;
+	 
+	 cout<<"time needed in days is "<<timeInDay<<endl;
+     
+	 return 0;
+	 
+}
