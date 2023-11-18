13fb625b38068402bdac75c3551c719b623b0b89
Kaleb Dagnachew <149406171+Kaleb-dag@users.noreply.github.com>
2023-11-18 03:22:58 -0800

    BMI calculator.cpp

--- ETS0747_15_Kaleb_Dagnachew/Activity 2.2/BMI calculator/BMI calculator.cpp
+++ ETS0747_15_Kaleb_Dagnachew/Activity 2.2/BMI calculator/BMI calculator.cpp
@@ -1,1 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    double weight, height;
+    cout << "Enter your weight (in killograms)";
+    cin >> weight;
+    cout << "Enter your height (in meters)";
+    cin >> height;
+    double bmi =weight/(height*height);
+    cout << "Your BMI result is" << bmi;
+    return 0;
+}
+
 
