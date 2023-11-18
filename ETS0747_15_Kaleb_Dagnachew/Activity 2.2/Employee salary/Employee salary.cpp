328b077d6fa2e39cbbc7c3c9a3219801a4e100ca
Kaleb Dagnachew <149406171+Kaleb-dag@users.noreply.github.com>
2023-11-18 02:29:35 -0800

    Employee salary.cpp

--- ETS0747_15_Kaleb_Dagnachew/Activity 2.2/Employee salary/Employee salary.cpp
+++ ETS0747_15_Kaleb_Dagnachew/Activity 2.2/Employee salary/Employee salary.cpp
@@ -1,31 +1,45 @@
- string employeeName;
-    double weeklyWorkingHours, bonusRate, ratePerHour, baseSalary;
-    double grossSalary, bonusPayment, netSalary;
+#include<iostream>
 
-    cout << "Enter employee name: ";
-    cin >> employeeName;
+using namespace std;
 
-    cout << "Enter weekly working hours: ";
-    cin >> weeklyWorkingHours;
-
-    cout << "Enter bonus rate: ";
-    cin >> bonusRate;
-
-    cout << "Enter rate per hour: ";
-    cin >> ratePerHour;
-
-    cout << "Enter base salary: ";
-    cin >> baseSalary;
-
-    grossSalary = weeklyWorkingHours * ratePerHour;
-
-    bonusPayment = grossSalary * bonusRate;
-
-    netSalary = grossSalary * 0.85;
-
-    cout << "\nEmployee: " << employeeName << endl;
-    cout << "Gross Salary: $" << grossSalary << endl;
-    cout << "Bonus Payment: $" << bonusPayment << endl;
-    cout << "Net Salary (after tax): $" << netSalary << endl;
-
-    return 0;
+int main()
+{ 
+   string employee_name;
+   
+   double work_hour, base_salary, bonus_rate, gross_salary,bonus_payment,net_salary,pension_rate(0.05),tax_rate(0.15);
+   
+     cout<<"enter the your full name"<<endl;
+   
+     getline (cin, employee_name);
+   
+     cout<<"enter the amount of hour you have worked"<<endl;
+   
+     cin>>work_hour;
+   
+     cout<<"enter your base salary"<<endl;
+   
+     cin>>base_salary;
+   
+      cout<<"enter your bonus rate per hour"<<endl;
+   
+      cin>>bonus_rate;
+      
+      gross_salary=base_salary+ (work_hour*bonus_rate*4);
+      // 4  was used because there are 4 weeks in a month
+     
+	  net_salary=gross_salary-(gross_salary*pension_rate)-(gross_salary* tax_rate);
+	  
+	  bonus_payment=work_hour*bonus_rate*4;
+	  
+	  cout<<employee_name<<endl;
+	  
+	  cout<<"your gross salary for this month is "<<gross_salary<<endl;
+	  
+	  cout<<"your net salary for this month is "<<net_salary<<endl;
+	  
+	  cout<<"your bonus payment for this month is "<<bonus_payment<<endl;
+    
+	 
+	 return 0;
+	 
+}
