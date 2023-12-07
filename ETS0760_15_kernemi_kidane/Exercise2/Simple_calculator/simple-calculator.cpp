e957b696e60c533db2857f2a507301d2869e9bae
kernemi <149039271+kernemi@users.noreply.github.com>
2023-12-07 22:56:23 +0300

    Add files via upload

---
+++ ETS0760_15_kernemi_kidane/Exercise2/Simple_calculator/simple-calculator.cpp
@@ -0,0 +1,49 @@
+//simple calculator
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    char op;
+    double num1, num2, result;
+
+    do {
+        cout << "Enter operator (+, -, *, /) or 0 to exit: ";
+        cin >> op;
+
+        if (op == '0') {
+            cout << "Exit" << endl;
+            break;
+        }
+
+        cout << "Enter two operands: ";
+        cin >> num1 >> num2;
+
+        switch (op) {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                if (num2 != 0) {
+                    result = num1 / num2;
+                } else {
+                    cout << "Error" << endl;
+                    continue;  
+                }
+                break;
+            default:
+                cout << "Error" << endl;
+                continue;  }
+
+        cout << "Result: " << result << endl;
+
+    } while (true);
+
+    return 0;
+}
