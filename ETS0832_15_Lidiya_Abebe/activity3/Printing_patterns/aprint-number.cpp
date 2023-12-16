a63f1a7fa3db4d2d945ae950b0faf71ba9b7824e
Lidiya Abebe <117913089+Sinuabe96@users.noreply.github.com>
2023-12-16 10:56:18 +0300

    Update aprint-number.cpp

--- ETS0832_15_Lidiya_Abebe/activity3/Printing_patterns/aprint-number.cpp
+++ ETS0832_15_Lidiya_Abebe/activity3/Printing_patterns/aprint-number.cpp
@@ -1,1 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+    for (int i = 10; i <= 49; ++i) {
+        cout << i << " ";
+        
+        // Move to the next line after every 10 numbers
+        if (i % 10 == 9 || i == 49) {
+            cout <<endl;
+        }
+    }
 
+    return 0;
+}
