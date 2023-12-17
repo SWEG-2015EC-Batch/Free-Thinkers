fe556cc96106f9e33edd0dc2e2673fde2fd95f08
Kaleb Dagnachew <149406171+Kaleb-dag@users.noreply.github.com>
2023-12-17 04:54:52 -0800

    Basic calculator.cpp

---
+++ ETS0747_15_Kaleb_Dagnachew/Activity3.1/Basic calculator/Basic calculator.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+int stop;
+    do {
+
+    float operation, num1, num2;
+    char opr; //operators
+    cout << "Enter first number: ";
+    cin >> num1;
+    cout << "Enter second number: ";
+    cin >> num2;
+    cout << "\nEnter an operator(\"+, *, -, /\"):";
+    cin >> opr;
+
+    switch(opr) {
+    case '+':
+        operation = num1 + num2;
+        cout << "\nThe sum of the number " << num1 << " and " << num2 << " is " << operation;
+        break;
+    case '-':
+        operation = num1 - num2;
+        cout << "The subtraction of the number " << num1 << " and " << num2 << " is " << operation;
+        break;
+    case '/':
+        operation = num1 / num2;
+        cout << "The division of the number " << num1 << " and " << num2 << " is " << operation;
+        break;
+    case '*':
+        operation = num1 * num2;
+        cout << "The multiplication of the number " << num1 << " and " << num2 << " is " << operation;
+        break;
+    default:
+        cout << "You enter the wrong operand" << endl;
+    }
+    cout << "\n\nEnter 0 to stop or any number to continue: ";
+    cin >> stop;
+    cout << "\n\n";
+    cout << "\033[2J\033[1;1H";
+
+
+    } while(stop != 0);
+    return 0;
+}
