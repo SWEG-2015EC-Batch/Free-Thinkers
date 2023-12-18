beb5a61445eb537efd67f80bce4532b2ca1e69f9
Kaleb Dagnachew <149406171+Kaleb-dag@users.noreply.github.com>
2023-12-18 05:14:45 -0800

    Case checker.cpp

--- ETS0747_15_Kaleb_Dagnachew/Activity3.1/Case checker /Case checker.cpp
+++ ETS0747_15_Kaleb_Dagnachew/Activity3.1/Case checker /Case checker.cpp
@@ -2,66 +2,24 @@
 using namespace std;
 
 int main() {
+    char ch;
 
-    char c;
     cout << "Enter a character: ";
-    cin >> c;
+    cin >> ch;
 
-     if (c >= '0' && c <= '9') {
-        cout << c << " is a digit." << endl;
-        switch(c){
-        case '0':
-            cout << "0 is neither even or odd" << endl;
-            break;
-        case '2':
-            cout << c << " is an even number" << endl;
-            break;
-        case '4':
-            cout << c << " is an even number" << endl;
-            break;
-        case '6':
-            cout << c << " is an even number" << endl;
-            break;
-        case '8':
-            cout << c << " is an even number" << endl;
-            break;
-        default:
-            cout << c << " is an odd number" << endl;
-
-    }
-    } else if (c >= 'A' && c <= 'Z') {
-        cout << c << " is an uppercase letter." << endl;
-        switch(c) {
-        case 'A':
-        case 'E':
-        case 'I':
-        case 'O':
-        case 'U':
-            cout << c << " is a vowel letter." << endl;
-            break;
-        default:
-            cout << c << " is a consonant letter." << endl;
-
+    if (isalpha(ch)) {
+        if (isupper(ch)) {
+            cout << "The entered character is an uppercase alphabet." << endl;
+        } else {
+            cout << "The entered character is a lowercase alphabet." << endl;
         }
-    } else if (c >= 'a' && c <= 'z') {
-        cout << c << " is a lowercase letter." << endl;
-        switch(c) {
-        case 'a':
-        case 'e':
-        case 'i':
-        case 'o':
-        case 'u':
-            cout << c << " is a vowel letter." << endl;
-            break;
-        default:
-            cout << c << " is a consonant letter." << endl;
-
-        }
-    } else {
-        cout << c << " is a special character." << endl;
+    } 
+    else if (isdigit(ch)) {
+        cout << "The entered character is a digit." << endl;
+    } 
+    else {
+        cout << "The entered character is a special character." << endl;
     }
-
-
 
     return 0;
 }
