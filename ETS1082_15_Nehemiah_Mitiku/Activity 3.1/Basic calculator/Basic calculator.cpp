bec111a18906e264666896e3599d04c8bf46d537
Nehemiah Mitiku <136116620+NvD-10@users.noreply.github.com>
2023-12-19 23:01:33 -0800

    Basic calculator.cpp

--- ETS1082_15_Nehemiah_Mitiku/Activity 3.1/Basic calculator/Basic calculator.cpp
+++ ETS1082_15_Nehemiah_Mitiku/Activity 3.1/Basic calculator/Basic calculator.cpp
@@ -1,1 +1,45 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int choice;
+    do{
+
+        float operation, num1, num2;
+        char opr;
+        cout << "Enter first number: ";
+        cin >> num1;
+        cout << "Enter second number: ";
+        cin >> num2;
+        cout << "Enter an operator(+, *, -, /): ";
+        cin >> opr;
+
+        switch(opr) {
+        case '+':
+            operation = num1 + num2;
+            cout << "\nThe sum is " << operation;
+            break;
+        case '-':
+            operation = num1 - num2;
+            cout << "\nThe difference is " << operation;
+            break;
+        case '/':
+            operation = num1 / num2;
+            cout << "\nThe quotient is " << operation;
+            break;
+        case '*':
+            operation = num1 * num2;
+            cout << "\nThe product is " << operation;
+            break;
+        default:
+            cout << "\nYou have entered an invalid operator" << endl;
+        }
+        cout << "\n\nEnter 0 to stop or any number to continue: ";
+        cin >> choice;
+        cout << "\n";
+    }while(choice != 0);
+
+    return 0;
+}
 
