6cf5c5b62d36e90abf7806b81acdbdbd2870264b
kernemi <149039271+kernemi@users.noreply.github.com>
2024-01-13 18:22:22 +0300

    Create Q3.cpp

---
+++ ETS0760_15_kernemi_kidane/Activity4.2/string/Q3.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cctype>
+using namespace std;
+
+int main() {
+    string input;
+    cout << "Enter a string: ";
+    getline(cin, input);
+
+    string result = "";
+
+    for (char character : input) {
+        if (isalpha(character)) {
+            result += character;
+        }
+    }
+
+    cout << "String after removing non-alphabet characters: " << result << endl;
+
+    return 0;
+}
