894da1188b06edc38720800282663b8fbae29646
Lidia Aliso <149233506+lydia-jc@users.noreply.github.com>
2024-01-14 19:47:33 +0300

    Q2.cpp

---
+++ ETS0830_15_Lidia_Aliso/Activity4.2/2D/Q2.cpp
@@ -0,0 +1,31 @@
+/*a C++ program to that accepts a two-dimensional array and displays the multiplication
+of row elements and the sum of column elements.*/
+#include <iostream>
+using namespace std;
+int main(){
+   int sample[5][5];//5 can be change accord to data
+   for(int i=0;i<5;++i){
+     for(int j=0;j<5;++j){
+    cout<<"enter the "<<i+1<<" row and "<<j+1<<" coloumn number: ";
+    cin>>sample[i][j];
+     }
+     }
+     cout << "Multiplication of row elements:\n";
+    for (int i = 0; i <5; ++i) {
+        int rowProduct = 1;
+        for (int j = 0; j <5; ++j) {
+            rowProduct *= sample[i][j];
+        }
+        cout << "Row " << i + 1 << ": " << rowProduct << "\n";
+    }
+     cout << "Sum of column elements:\n";
+    for (int j = 0; j <5; ++j) {
+        int colSum = 0;
+        for (int i = 0; i <5; ++i) {
+            colSum += sample[i][j];
+        }
+        cout << "Column " << j + 1 << ": " << colSum << "\n";
+    }
+     return 0;
+}
+   
